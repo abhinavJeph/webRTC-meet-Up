5f68857510716f5ffb476c0d9e0c23723b98cc25
abhinavjeph <abhinavmeenameena@gmail.com>
2021-07-11 14:26:47 +0530

    create basic socket.io server

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
+const io = require('socket.io')(server);
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "public/index.html");
+})
+
+io.on("connection", (socket) =>{
+    console.log("User connected on socket.io server");
+    console.log(socket.id);
 })
 
 server.listen(PORT, () => {
