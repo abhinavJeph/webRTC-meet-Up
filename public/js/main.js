e2d84b26f81556bd96ea48d553ed18cb15780285
abhinavjeph <abhinavmeenameena@gmail.com>
2021-07-11 21:00:40 +0530

    register event for personal connection buttons

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,33 @@
 import * as store from "./store.js";
 import * as wss from "./wss.js";
+import * as constants from "./constants.js";
+import * as webRtcHandler from "./webRtcHandler.js";
 
 // initialization of socket.io connection
 const socket = io("/");
 wss.registerSocketEvents(socket);
 
-// register event for personalCode copy button
+// register event listner for personalCode copy button
 const personalCodeCopyButton = document.getElementById("personal_code_copy_button");
 personalCodeCopyButton.addEventListener("click", () =>{
   const personalCode = store.getState().socketId;
   navigator.clipboard && navigator.clipboard.writeText(personalCode);
 })
 
+// register event listeners for connection call_buttons
+const personalCodeChatButton = document.getElementById("personal_code_chat_button");
+const personalCodeVideoButton = document.getElementById("personal_code_video_button");
+
+personalCodeChatButton.addEventListener("click", () =>{
+  const calleePersonalCode = document.getElementById("personal_code_input").value;
+  const callType = constants.CALL_TYPE.CHAT_PERSONAL_CODE;
+
+  webRtcHandler.sendPreOffer(callType, calleePersonalCode);
+})
+
+personalCodeVideoButton.addEventListener("click", () =>{
+  const calleePersonalCode = document.getElementById("personal_code_input").value;
+  const callType = constants.CALL_TYPE.VIDEO_PERSONAL_CODE;
+
+  webRtcHandler.sendPreOffer(callType, calleePersonalCode);
+})
---
+++ public/js/webRTCHandler.js
@@ -0,0 +1,4 @@
+export const sendPreOffer = (callType, calleePersonalCode) => {
+    console.log('sending pre call offer');
+    console.log(callType + " " + calleePersonalCode);
+}
