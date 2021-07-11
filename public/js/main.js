f0053c0f094af45f9733b7df7c83e02b665e7fd9
abhinavjeph <abhinavmeenameena@gmail.com>
2021-07-11 20:36:55 +0530

    add copy to clipboard button for personal code

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,14 @@
 import * as store from "./store.js";
 import * as wss from "./wss.js";
 
+// initialization of socket.io connection
 const socket = io("/");
 wss.registerSocketEvents(socket);
+
+// register event for personalCode copy button
+const personalCodeCopyButton = document.getElementById("personal_code_copy_button");
+personalCodeCopyButton.addEventListener("click", () =>{
+  const personalCode = store.getState().socketId;
+  navigator.clipboard && navigator.clipboard.writeText(personalCode);
+})
 
